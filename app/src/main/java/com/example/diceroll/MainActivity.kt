1d874643e6edb962aa9ac30ba74c845bd2f57994
Kaio Masaru Nakazono <kaio.webtech@gmail.com>
2021-08-02 10:11:51 -0300

    primeiro commit

---
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.diceroll
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            //Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+            val resultTextView: TextView = findViewById(R.id.textView)
+            resultTextView.text = "6"
+        }
+    }
+}
